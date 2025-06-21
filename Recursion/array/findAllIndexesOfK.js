e55978c20cd3f926e3197d16d8da43a035dfbf09
Ravindra Singh Shah <ravindrasinghshah@gmail.com>
2025-06-21 17:05:22 -0500

    refactor: optimize findAllIndexesOfK function to improve recursion handling

--- Recursion/array/findAllIndexesOfK.js
+++ Recursion/array/findAllIndexesOfK.js
@@ -2,12 +2,29 @@
  * cd to Recursion/array and run node findAllIndexesOfK.js
  */
 
+// function findAllIndexesOfK(nums, target) {
+//     function findElement(arr, i, indexes) {
+//         //== base condition: traversed all indexes
+//         if (i >= arr.length) return indexes;
+//         if (arr[i] === target) indexes.push(i);
+//         return findElement(arr, ++i, indexes);
+//     }
+//     let allIndexes = findElement(nums, 0, []);
+//     return allIndexes;
+// }
+
+//== if want to use list within body of recursion, then have to return back and update the calling function list
+//==== this is not optimized approach because list is created for each recursive calls.
 function findAllIndexesOfK(nums, target) {
-    function findElement(arr, i, indexes) {
+    function findElement(arr, i) {
+        let list = [];
         //== base condition: traversed all indexes
-        if (i >= arr.length) return indexes;
-        if (arr[i] === target) indexes.push(i);
-        return findElement(arr, ++i, indexes);
+        if (i >= arr.length) return list;
+        //== this will only contain answer for this function call only
+        if (arr[i] === target) list.push(i);
+        let retList = findElement(arr, ++i);
+
+        return [...list, ...retList];
     }
     let allIndexes = findElement(nums, 0, []);
     return allIndexes;
