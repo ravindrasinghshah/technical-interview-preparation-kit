84cbed2d8abb13c487f2cfc74b5a735d5d9ca77e
Ravindra Singh Shah <ravindrasinghshah@gmail.com>
2025-06-28 09:05:23 -0500

    doubly linked list

--- DoublyLinkedList/index.js
+++ DoublyLinkedList/index.js
@@ -19,6 +19,15 @@ class DoublyLinkedList {
         this.tail.prev = this.head;
     }
     isEmpty() { return this.head === null; }
+    insertFirst(node) {
+        if (this.head.data === -1) {
+            this.head.data = node.data;
+        } else {
+            node.next = this.head;
+            this.head.prev = node;
+            this.head = node;
+        }
+    }
     add(node) {
         const h_next = this.head.next;
         h_next.prev = node;
@@ -26,18 +35,46 @@ class DoublyLinkedList {
         node.prev = this.head;
         this.head.next = node;
     }
+    delete(value) {
+        let curr = this.head;
+        while (curr) {
+            if (curr.data === value) {
+                if (curr.prev) {
+                    curr.prev.next = curr.next;
+                }
+                else curr = curr.next;
+                return;
+            }
+            curr = curr.next;
+        }
+    }
+    contains(value) {
+        let slow = this.head; let fast = this.head;
+        while (fast && fast.next) {
+            if (fast.data === value || slow.data === value) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+    print() {
+        let curr = this.head;
+        while (curr) {
+            console.log(curr.data);
+            curr = curr.next;
+        }
+    }
 }
 
 const list = new DoublyLinkedList(2);
-console.log("list head and next", list.head);
-let node = new Node(1);
-list.add(node);
-console.log("list head and next", list.head);
 
-node = new Node(2);
-list.add(node);
-console.log("list head and next", list.head);
-
-node = new Node(3);
-list.add(node);
-console.log("list head and next", (list));
+list.insertFirst(new Node('a'));
+list.insertFirst(new Node('b'));
+list.insertFirst(new Node('c'));
+list.insertFirst(new Node('d'));
+list.insertFirst(new Node('e'));
+list.print();
+console.log('Does list contains "d"?', list.contains('d'));
+console.log('Does list contains "f"?', list.contains('f'));
+list.delete('a');
+list.print();
