0a5be9fd78267abc3983215b06b10169a88510fa
Ravindra Singh Shah <ravindrasinghshah@gmail.com>
2025-08-29 12:07:14 -0500

    log warn

--- Log.ts
+++ Log.ts
@@ -3,9 +3,8 @@ type MessageType = string | number | Array<string | number>;
 export class Log {
   private static colors = {
     green: "\x1b[32m",
-    orange: "\x1b[33m", // no real orange, closest is yellow
+    orange: "\x1b[33m",
     blue: "\x1b[34m",
-    bold: "\x1b[1m",
     reset: "\x1b[0m",
   };
 
@@ -17,7 +16,6 @@ export class Log {
   }
 
   static warn(message: MessageType, ...args: any[]): void {
-    console.warn(`%cWARN: ${message}`, Log.colors.orange, ...args);
     console.log(
       `${Log.colors.orange}WARN:${Log.colors.reset} ${message}`,
       ...args
